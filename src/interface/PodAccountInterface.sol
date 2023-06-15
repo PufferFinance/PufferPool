b6160ebfc635bf9ee865c37c0b660905a4112643
Jason Vranek <jvranek@ucsc.edu>
2023-06-14 18:20:37 -0700

    Add PufferPool.sol

--- src/interface/PodAccountInterface.sol
+++ src/interface/PodAccountInterface.sol
@@ -4,40 +4,34 @@ pragma solidity ^0.8.13;
 interface PodAccountInterface {
     // Meant to be called in constructor
     function initialize(
-        bytes[] memory podEnclavePubKeys, 
-        address[] memory podWallets, 
-        bytes32 podType) 
-        external;
+        bytes[] memory podEnclavePubKeys,
+        address[] memory podWallets,
+        bytes32 podType
+    ) external;
 
     // PodAccount posts a new validator key, required metadata, and their bond
-	function registerValidatorKey(
-        bytes memory pubKey, 
-        bytes[] memory pubKeyShares, 
-        bytes[] memory encKeyShares, 
-        bytes memory depositDataRoot, 
+    function registerValidatorKey(
+        bytes memory pubKey,
+        bytes[] memory pubKeyShares,
+        bytes[] memory encKeyShares,
+        bytes memory depositDataRoot,
         bytes memory depositSignature,
-        bytes[] memory podSignatures)
-        external payable returns(bytes32 withdrawalCredentials);
+        bytes[] memory podSignatures
+    ) external payable returns (bytes32 withdrawalCredentials);
 
     // PodAccount can opt-in to restake at target contract
-	function restake(
-        address targetContract) 
-        external payable returns(bool success);
+    function restake(
+        address targetContract
+    ) external payable returns (bool success);
 
     // PodAccount can post data to the target contract (e.g., stateroot to rollup bridge contract)
-	function postToRestakingService(
-        address targetContract, 
-        bytes memory payload, 
-        bytes[] memory podSignatures) 
-        external;
-
-    function withdraw(
+    function postToRestakingService(
         address targetContract,
-        address balanceReceiver) 
-        external;
+        bytes memory payload,
+        bytes[] memory podSignatures
+    ) external;
 
-    function skim(
-        address targetContract,
-        address balanceReceiver) 
-        external;
+    function withdraw(address targetContract, address balanceReceiver) external;
+
+    function skim(address targetContract, address balanceReceiver) external;
 }
--- src/interface/PufferPoolInterface.sol
+++ src/interface/PufferPoolInterface.sol
@@ -2,82 +2,83 @@
 pragma solidity ^0.8.13;
 
 interface PufferPoolInterface {
-	function registerPod(bytes[] memory raEvidences, bytes[] memory ethPks, uint256[] memory regBlockNums, bytes32 mrenclave) external returns(bool success);
+    function registerPod(
+        bytes[] memory raEvidences,
+        bytes[] memory ethPks,
+        uint256[] memory regBlockNums,
+        bytes32 mrenclave
+    ) external returns (bool success);
 
-    function registerCrew(bytes[] memory raEvidences, bytes[] memory ethPks, uint256[] memory regBlockNums, bytes32 mrenclave) external returns(bool success);
+    function registerCrew(
+        bytes[] memory raEvidences,
+        bytes[] memory ethPks,
+        uint256[] memory regBlockNums,
+        bytes32 mrenclave
+    ) external returns (bool success);
 
-    function upgradeCrew(address newCrewAddress) external returns(bool success);
+    function upgradeCrew(
+        address newCrewAddress
+    ) external returns (bool success);
 
     function registerValidatorKey(
-        bytes memory pubKey, 
-        bytes[] memory pubKeyShares, 
-        bytes[] memory encKeyShares, 
-        bytes memory depositDataRoot, 
+        bytes memory pubKey,
+        bytes[] memory pubKeyShares,
+        bytes[] memory encKeyShares,
+        bytes memory depositDataRoot,
         bytes memory depositSignature,
         bytes[] memory podSignatures,
-        bytes32 podType)
-        external payable returns(bytes32 withdrawalCredentials);
+        bytes32 podType
+    ) external payable returns (bytes32 withdrawalCredentials);
 
-	function provisionPod(
-        bytes memory pubKey, 
-        bytes memory depositDataRoot, 
+    function provisionPod(
+        bytes memory pubKey,
+        bytes memory depositDataRoot,
         bytes memory depositSignature,
         bytes[] memory crewSignatures,
-        bytes32 podType)
-        external returns(bool success);
-    
-	function approveRestakeRequest(
+        bytes32 podType
+    ) external returns (bool success);
+
+    function approveRestakeRequest(
         address targetContract,
-        bytes32 podType) 
-        external payable returns(bool success);
+        bytes32 podType
+    ) external payable returns (bool success);
 
-	function calcWithdrawalCredentials(
-        bytes memory pubKey) 
-        external pure returns(address withdrawalCredentials);
-    
+    function calcWithdrawalCredentials(
+        bytes memory pubKey
+    ) external pure returns (address withdrawalCredentials);
+
     function ejectPodForInactivity(
-        address podAccount, 
-        bytes32 podType, 
-        bytes32 beaconStateRoot, 
-        uint256 validatorIndex, 
-        bytes[] memory crewSignatures)
-        external;
+        address podAccount,
+        bytes32 podType,
+        bytes32 beaconStateRoot,
+        uint256 validatorIndex,
+        bytes[] memory crewSignatures
+    ) external;
 
-	function ejectPodForTheft(
-        address podAccount, 
-        bytes32 podType, 
-        bytes32 beaconStateRoot, 
-        uint256 validatorIndex, 
-        bytes memory validatorPubKey, 
-        bytes[] memory crewSignatures)
-        external;
+    function ejectPodForTheft(
+        address podAccount,
+        bytes32 podType,
+        bytes32 beaconStateRoot,
+        uint256 validatorIndex,
+        bytes memory validatorPubKey,
+        bytes[] memory crewSignatures
+    ) external;
 
     // LST related
     function mint(address recipient) external payable;
+
     function redeem(address recipient) external payable;
 
     // Contract maintanence
     function pause() external;
+
     function resume() external;
+
     function upgrade(address newContractAddr) external;
 
     // Setters to set parameters
     function setParamX() external;
 
     // Getters to get parameters
-    function getParamX() 
-        external returns(uint256 X);
-}
-
-abstract contract PufferPoolInternals is PufferPoolInterface {
-    // Internal helper functions that can't be expressed in the interface
-	function extractEnclaveEthKeys(bytes[] memory payloads) internal virtual returns (bytes memory pubKeys);
-
-	function decodeToEthPubkey(bytes memory enclavePayload) internal virtual pure returns (bytes memory pubKey);
-
-    function crewAccountFactory(bytes[] memory crewEnclavePubKeys, address[] memory crewWallets, bytes32 mrenclave) internal virtual returns(address accountAddress);
-
-    function podAccountFactory(bytes[] memory podEnclavePubKeys, address[] memory podWallets, bytes32 mrenclave) internal virtual returns(address accountAddress);
-
-    function splitterContractFactory(bytes32 seed) internal virtual returns(address contractAddress);
+    function getParamX() external returns (uint256 X);
 }
