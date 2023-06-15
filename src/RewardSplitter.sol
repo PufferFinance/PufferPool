b210cdab94c4bd0dedcd4b4ef1f484e95edf68b3
Jason Vranek <jvranek@ucsc.edu>
2023-06-14 18:38:31 -0700

    Add skeleton contracts

---
+++ src/RewardSplitter.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.13;
+
+import "src/interface/RewardSplitterInterface.sol";
+
+abstract contract RewardSplitterBase is RewardSplitterInterface {}
---
+++ test/RewardSplitter.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+
+import "src/RewardSplitter.sol";
+
+contract TestContract is Test {
+    Contract c;
+
+    function setUp() public {}
+}
