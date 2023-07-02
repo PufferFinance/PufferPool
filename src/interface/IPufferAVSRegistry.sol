ad1d9ac7540cdd3f4324843710a6c42874055ebe
CheyenneAtapour <cheyenneatapour@gmail.com>
2023-07-02 12:44:41 -0700

    remove unnecessary functions from avs registry interface

--- src/interface/IPufferAVSRegistry.sol
+++ src/interface/IPufferAVSRegistry.sol
@@ -4,10 +4,4 @@ pragma solidity >=0.8.0 <0.9.0;
 interface IPufferAVSRegistry {
     /// @notice Returns True if the `operator` is "registered" and thus an active operator
     function operatorActive(address operator) external view returns (bool);
-
-    /// @notice Registers an operator
-    function registerOperator(bytes calldata registrationData) external;
-
-    /// @notice Deregisters an operator
-    function deregisterOperator(bytes calldata deregistrationData) external;
 }
