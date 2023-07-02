7c4399ef8ada69c1450e87f065ac2fb8701b353d
CheyenneAtapour <cheyenneatapour@gmail.com>
2023-07-02 13:24:19 -0700

    add puffer avs interface

--- src/interface/IPufferAVS.sol
+++ src/interface/IPufferAVS.sol
@@ -2,4 +2,12 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 interface IPufferAVS {
+	/// @notice Freezes operator, 
+	function haltOperator(address operator) external;
+
+	function recordInitialStakeUpdate(address operator, uint32 serveUntil) external;
+
+	function recordFinalStakeUpdateRevokeSlashing(address operator, uint33 serveUntil) external;
+
+	function recordStakeUpdate(address operator, uint32 updateBlock, uint32 serveUntil, uint256 prevElement) external;
 }
