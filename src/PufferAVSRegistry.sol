4b7cba1f8448903dcb2409a7aa737c92719a2ff0
CheyenneAtapour <cheyenneatapour@gmail.com>
2023-07-04 12:10:02 -0700

    run forge fmt

--- src/PufferAVSRegistry.sol
+++ src/PufferAVSRegistry.sol
@@ -6,21 +6,13 @@ import "eigenlayer/middleware/RegistryBase.sol";
 import "./interface/IPufferAVSRegistry.sol";
 
 contract PufferAVSRegistry is RegistryBase, IPufferAVSRegistry {
-
-    constructor (
-        IStrategyManager _strategyManager,
-        IServiceManager _serviceManager,
-        uint8 _NUMBER_OF_QUORUMS
-    ) RegistryBase(
-            _strategyManager,
-            _serviceManager,
-            _NUMBER_OF_QUORUMS
-        ) 
-    {}
+    constructor(IStrategyManager _strategyManager, IServiceManager _serviceManager, uint8 _NUMBER_OF_QUORUMS)
+        RegistryBase(_strategyManager, _serviceManager, _NUMBER_OF_QUORUMS)
+    { }
 
     /// @notice Returns True if the `operator` is "registered" and thus an active operator
     function operatorActive(address operator) external view returns (bool) {
-    	return (registry[operator].status == IQuorumRegistry.Status.ACTIVE);
+        return (registry[operator].status == IQuorumRegistry.Status.ACTIVE);
     }
 
     function register(address operator, uint8 operatorType, bytes32 pubkeyHash) external {
