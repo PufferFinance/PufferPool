edfc65be521d1a143dc1ae26f48d2c88886b767e
JasonVranek <jvranek@ucsc.edu>
2023-08-22 20:13:57 -0700

    Clean up EnclaveVerifier to just expose one generalized function verifyEvidence()

--- src/EnclaveVerifier.sol
+++ src/EnclaveVerifier.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity >=0.8.0 <0.9.0;
 
-import { RAVE } from "rave/RAVE.sol";
-import { X509Verifier } from "rave/X509Verifier.sol";
-import { IEnclaveVerifier } from "puffer/interface/IEnclaveVerifier.sol";
-import { RaveEvidence } from "puffer/interface/RaveEvidence.sol";
+import {RAVE} from "rave/RAVE.sol";
+import {X509Verifier} from "rave/X509Verifier.sol";
+import {IEnclaveVerifier} from "puffer/interface/IEnclaveVerifier.sol";
+import {RaveEvidence} from "puffer/interface/RaveEvidence.sol";
 
 /**
  * @title EnclaveVerifier
@@ -13,8 +13,7 @@ import { RaveEvidence } from "puffer/interface/RaveEvidence.sol";
  */
 contract EnclaveVerifier is IEnclaveVerifier, RAVE {
     /**
-     * @dev RSA Public key for Intel
-     *      TODO: LINK to where this can be found online, on official Intel site
+     * @dev RSA Public key for Intel: https://api.portal.trustedservices.intel.com/documentation
      */
     bytes internal constant _INTEL_RSA_MODULUS =
         hex"9F3C647EB5773CBB512D2732C0D7415EBB55A0FA9EDE2E649199E6821DB910D53177370977466A6A5E4786CCD2DDEBD4149D6A2F6325529DD10CC98737B0779C1A07E29C47A1AE004948476C489F45A5A15D7AC8ECC6ACC645ADB43D87679DF59C093BC5A2E9696C5478541B979E754B573914BE55D32FF4C09DDF27219934CD990527B3F92ED78FBF29246ABECB71240EF39C2D7107B447545A7FFB10EB060A68A98580219E36910952683892D6A5E2A80803193E407531404E36B315623799AA825074409754A2DFE8F5AFD5FE631E1FC2AF3808906F28A790D9DD9FE060939B125790C5805D037DF56A99531B96DE69DE33ED226CC1207D1042B5C9AB7F404FC711C0FE4769FB9578B1DC0EC469EA1A25E0FF9914886EF2699B235BB4847DD6FF40B606E6170793C2FB98B314587F9CFD257362DFEAB10B3BD2D97673A1A4BD44C453AAF47FC1F2D3D0F384F74A06F89C089F0DA6CDB7FCEEE8C9821A8E54F25C0416D18C46839A5F8012FBDD3DC74D256279ADC2C0D55AFF6F0622425D1B";
@@ -51,77 +50,29 @@ contract EnclaveVerifier is IEnclaveVerifier, RAVE {
      * @inheritdoc IEnclaveVerifier
      */
     function getIntelRootCAPubKey() public pure returns (RSAPubKey memory) {
-        return RSAPubKey({ modulus: _INTEL_RSA_MODULUS, exponent: _INTEL_EXPONENT });
-    }
-
-    /**
-     * @inheritdoc IEnclaveVerifier
-     */
-    function verifyGuardianPubKey(
-        bytes calldata pubKey,
-        uint256 blockNumber,
-        RaveEvidence calldata evidence,
-        bytes32 mrenclave,
-        bytes32 mrsigner
-    ) external view returns (bool) {
-        // Check for freshness
-        if ((block.number - blockNumber) > FRESHNESS_BLOCKS) {
-            revert StaleEvidence();
-        }
-
-        // Enclave is expected to have hashed in this order
-        bytes32 digest = keccak256(abi.encode(blockhash(blockNumber), pubKey));
-
-        bytes memory payload = new bytes(64);
-
-        // Pad to 64B
-        assembly {
-            mstore(add(payload, 32), digest)
-        }
-
-        // Getting here means the remote attestation evidence was valid
-        return _verifyEvidence(payload, evidence, mrenclave, mrsigner);
-    }
-
-    /**
-     * @inheritdoc IEnclaveVerifier
-     */
-    function verifyValidatorPubKey(
-        bytes calldata pubKey,
-        uint256 blockNumber,
-        // TODO add the remaining fields
-        RaveEvidence calldata evidence,
-        bytes32 mrenclave,
-        bytes32 mrsigner
-    ) external view returns (bool) {
-        // Check for freshness
-        if ((block.number - blockNumber) > FRESHNESS_BLOCKS) {
-            revert StaleEvidence();
-        }
-
-        // Enclave is expected to have hashed in this order
-        bytes32 digest = keccak256(abi.encode(blockhash(blockNumber), pubKey)); // TODO this needs to be updated
-        bytes memory payload = new bytes(64);
-
-        // Pad to 64B
-        assembly {
-            mstore(add(payload, 32), digest)
-        }
-
-        // Getting here means the remote attestation evidence was valid
-        return _verifyEvidence(payload, evidence, mrenclave, mrsigner);
+        return
+            RSAPubKey({modulus: _INTEL_RSA_MODULUS, exponent: _INTEL_EXPONENT});
     }
 
     /**
      * @notice Adds a leaf x509 RSA public key if the x509 was signed by Intel's root CA
      */
     function addLeafX509(bytes calldata leafX509Cert) external onlyPool {
-        (bytes memory leafCertModulus, bytes memory leafCertExponent) =
-            X509Verifier.verifySignedX509(leafX509Cert, _INTEL_RSA_MODULUS, _INTEL_EXPONENT);
+        (
+            bytes memory leafCertModulus,
+            bytes memory leafCertExponent
+        ) = X509Verifier.verifySignedX509(
+                leafX509Cert,
+                _INTEL_RSA_MODULUS,
+                _INTEL_EXPONENT
+            );
 
         bytes32 hashedCert = keccak256(leafX509Cert);
 
-        _validLeafX509s[hashedCert] = RSAPubKey({ modulus: leafCertModulus, exponent: leafCertExponent });
+        _validLeafX509s[hashedCert] = RSAPubKey({
+            modulus: leafCertModulus,
+            exponent: leafCertExponent
+        });
 
         emit AddedPubKey(hashedCert);
     }
@@ -136,20 +87,39 @@ contract EnclaveVerifier is IEnclaveVerifier, RAVE {
         emit RemovedPubKey(hashedCert);
     }
 
-    function _verifyEvidence(
-        bytes memory expectedPayload,
+    /**
+     * @inheritdoc IEnclaveVerifier
+     */
+    function verifyEvidence(
+        uint256 blockNumber,
+        bytes32 raveCommitment,
         RaveEvidence calldata evidence,
         bytes32 mrenclave,
         bytes32 mrsigner
-    ) internal view returns (bool success) {
-        RSAPubKey memory leafX509 = _validLeafX509s[evidence.leafX509CertDigest];
+    ) external view returns (bool success) {
+        // Check for freshness
+        if ((block.number - blockNumber) > FRESHNESS_BLOCKS) {
+            revert StaleEvidence();
+        }
+
+        RSAPubKey memory leafX509 = _validLeafX509s[
+            evidence.leafX509CertDigest
+        ];
         // require(leafX509.modulus.length == todo);
         // require(leafX509.exponent == todo);
 
         // Recover a remote attestation payload if everything is valid
         bytes memory recoveredPayload = verifyRemoteAttestation(
-            evidence.report, evidence.signature, leafX509.modulus, leafX509.exponent, mrenclave, mrsigner
+            evidence.report,
+            evidence.signature,
+            leafX509.modulus,
+            leafX509.exponent,
+            mrenclave,
+            mrsigner
         );
+
+        // Remote attestation payloads are expected to be in the form (32B_Commitment || 32B_BlockHash)
+        bytes memory expectedPayload = abi.encode(raveCommitment, blockhash(blockNumber));
 
         // Compare with the expected payload
         return (keccak256(expectedPayload) == keccak256(recoveredPayload));
--- src/interface/IEnclaveVerifier.sol
+++ src/interface/IEnclaveVerifier.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity >=0.8.0 <0.9.0;
 
-import { RaveEvidence } from "puffer/interface/RaveEvidence.sol";
+import {RaveEvidence} from "puffer/interface/RaveEvidence.sol";
 
 /**
  * @title IEnclaveVerifier interface
@@ -47,33 +47,16 @@ interface IEnclaveVerifier {
     function getIntelRootCAPubKey() external pure returns (RSAPubKey memory);
 
     /**
-     * @notice Verifies the guardian public key
-     * @param pubKey is the guardian's public key
-     * @param blockNumber is the block number for whcih evidence is created
-     * @param evidence is the evidence
-     * @param mrenclave is the mrenclave value
-     * @param mrsigner is the mrsigner value
+     * @notice Verifies remote attestation evidence: the report contains the expected MRENCLAVE/MRSIGNER values, a valid TCB status, and was signed by an Intel-issued x509 certificate. The report will contain a 64B payload in the form (32B_Commitment || 32B_BlockHash), where 32B_Blockhash is a recent L1 blockhash and 32B_Commitment is a keccak256 hash that the enclave is committing to. The calling contract is expected to precompute raveCommitment from public inputs. The function returns true if the report is valid and the extracted payload matches the expected.
+     * @param blockNumber is the block number to fetch 32B_Blockhash
+     * @param raveCommitment is the keccak256 hash commitment 32B_Commitment
+     * @param evidence is the remote attestation evidence
+     * @param mrenclave is the MRENCLAVE value expected by the calling contract
+     * @param mrsigner is the MRSIGNER value expected by the calling contract
      */
-    function verifyGuardianPubKey(
-        bytes calldata pubKey,
+    function verifyEvidence(
         uint256 blockNumber,
-        RaveEvidence calldata evidence,
-        bytes32 mrenclave,
-        bytes32 mrsigner
-    ) external view returns (bool);
-
-    /**
-     * @notice Verifies the Validator public key
-     * @param pubKey is the Validator's public key
-     * @param blockNumber is the block number for whcih evidence is created
-     * @param evidence is the evidence
-     * @param mrenclave is the mrenclave value
-     * @param mrsigner is the mrsigner value
-     */
-    function verifyValidatorPubKey(
-        bytes calldata pubKey,
-        uint256 blockNumber,
-        // TODO add the remaining fields
+        bytes32 raveCommitment,
         RaveEvidence calldata evidence,
         bytes32 mrenclave,
         bytes32 mrsigner
