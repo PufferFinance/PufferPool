4942dbc158c0c74bc861904a265118624840b719
Benjamin <benjaminxh+github@gmail.com>
2023-09-18 15:39:08 +0200

    fix basescript

--- scripts/BaseScript.s.sol
+++ scripts/BaseScript.s.sol
@@ -8,10 +8,12 @@ import { Script } from "forge-std/Script.sol";
  * @author Puffer finance
  */
 abstract contract BaseScript is Script {
+    uint256 PK = 55358659325830545179143827536745912452716312441367500916455484419538098489698; // makeAddr("pufferDeployer")
+
     /**
      * @dev Deployer private key is in `PK` env variable
      */
-    uint256 _deployerPrivateKey = vm.envUint("PK");
+    uint256 _deployerPrivateKey = vm.envOr("PK", PK);
     address internal _broadcaster = vm.addr(_deployerPrivateKey);
 
     modifier broadcast() {
