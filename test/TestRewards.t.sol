ee02da16f9ce9bacbfe9ef33aa37d914fc561e68
Cheyenne Atapour <CheyenneAtapour@gmail.com>
2023-09-29 16:52:43 -0700

    add additional functions to test hard input of merkle proofs

--- test/TestRewards.t.sol
+++ test/TestRewards.t.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import { MerkleProof } from "openzeppelin/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract MerkleProofVerifier {
     bytes32 public merkleRoot;
@@ -19,13 +19,22 @@ contract MerkleProofVerifier {
         uint256 commission_dividend,
         uint256 commission_divisor,
         bytes32[] memory proof
-    ) public view returns (bytes32[] memory, bytes32, bytes32) { //(bool) {
+    ) public view returns (bytes32[] memory, bytes32, bytes32) { //(bool) { 
         // Construct the leaf from the account, validator pubkeys, and the provided data
-        bytes32 leaf = keccak256(abi.encodePacked(bytes("TODO: avsAddress"), concatValidatorPubkeys(validator_pubkeys), consensus_rewards_eth, commission_dividend, commission_divisor));
+        //bytes32 leaf = keccak256(abi.encodePacked(bytes("TODO: avsAddress"), concatValidatorPubkeys(validator_pubkeys), consensus_rewards_eth, commission_dividend, commission_divisor));
+        bytes32 leaf = keccak256(abi.encodePacked(consensus_rewards_eth, commission_dividend, commission_divisor));
 
         return (proof, merkleRoot, leaf);
         // Verify the proof against the merkle root
         //return MerkleProof.verify(proof, merkleRoot, leaf);
+    }
+
+    function checkProof(
+        bytes32[] memory proof,
+        bytes32 leaf
+    ) public view returns (bool)
+    {
+        return MerkleProof.verify(proof, merkleRoot, leaf);
     }
 
     // Helper function to concatenate validator pubkeys into a single bytes
