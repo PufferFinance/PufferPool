bef435ef12f495c5af7005c1fef6c4d8fed82491
Benjamin <benjaminxh+github@gmail.com>
2024-04-12 06:52:32 +0200

    update deposit contract for holesky

--- script/DeployPufferProtocolImplementation.s.sol
+++ script/DeployPufferProtocolImplementation.s.sol
@@ -28,7 +28,7 @@ contract DeployPufferProtocolImplementation is Script {
             guardianModule: GuardianModule(payable(0xD349FdCD0e4451381bfE7cba3ac28773E176b326)),
             moduleManager: address(0xe4695ab93163F91665Ce5b96527408336f070a71),
             oracle: IPufferOracleV2(0xEf93AA29F627465A7f58A1F25980c90116f27b74),
-            beaconDepositContract: 0x00000000219ab540356cBB839Cbe05303d7705Fa
+            beaconDepositContract: 0x4242424242424242424242424242424242424242
         });
     }
 }
